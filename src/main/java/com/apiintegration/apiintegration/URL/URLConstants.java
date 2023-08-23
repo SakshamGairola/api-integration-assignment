9a1ecb58c28eee9ece296e10c5b2164c97df3c86
Saksham <saksham.gairola28@gmail.com>
2023-08-23 23:22:25 +0530

    contains API url constants

---
+++ src/main/java/com/apiintegration/apiintegration/URL/URLConstants.java
@@ -0,0 +1,7 @@
+package com.apiintegration.apiintegration.URL;
+
+public class URLConstants {
+    public static final String BASE_URL = "https://qa2.sunbasedata.com/sunbase/portal/api/";
+    public static final String FETCH_ACCESS_TOKEN_ENDPOINT = "assignment_auth.jsp";
+    public static final String API_ENDPOINTS = "assignment.jsp";
+}
