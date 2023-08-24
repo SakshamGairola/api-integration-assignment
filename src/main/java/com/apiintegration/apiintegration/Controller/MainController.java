bd046df1f3a03fb50e171dfef26d7f27dc0e1dad
Saksham <saksham.gairola28@gmail.com>
2023-08-24 11:37:43 +0530

    gloablised accessToken

--- src/main/java/com/apiintegration/apiintegration/Controller/MainController.java
+++ src/main/java/com/apiintegration/apiintegration/Controller/MainController.java
@@ -1,8 +1,10 @@
 package com.apiintegration.apiintegration.Controller;
 
+import com.apiintegration.apiintegration.Entities.AccessToken;
 import com.apiintegration.apiintegration.Entities.Customer;
 import com.apiintegration.apiintegration.Entities.User;
 import com.apiintegration.apiintegration.Service.WebClientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,11 @@ public class MainController {
 
     @Autowired
     private WebClientService webClientService;
+
+    AccessToken accessToken;
+
+    @Autowired
+    ObjectMapper mapper;
 
     @RequestMapping("/")
     public String red() {
@@ -31,8 +38,12 @@ public class MainController {
 
     @PostMapping("/login-verify")
     public String verifyLogin(@ModelAttribute("user") User user) {
+        String accessTokenJson;
         try {
-            webClientService.fetchAccessToken(user);
+            accessTokenJson = webClientService.fetchAccessToken(user);
+            accessToken = mapper.readValue(accessTokenJson, AccessToken.class);
+            accessToken.setAccess_token("Bearer " + accessToken.getAccess_token());
+            System.out.println(accessTokenJson);
         } catch (Exception e) {
             e.printStackTrace();
             return "redirect:/login";
@@ -43,7 +54,7 @@ public class MainController {
     @GetMapping("view-all")
     public ModelAndView getAllCustomer() {
         ModelAndView mav = new ModelAndView("viewAll");
-        List<Customer> allCustomers = webClientService.getAllCustomer();
+        List<Customer> allCustomers = webClientService.getAllCustomer(accessToken);
         mav.addObject("allCustomers", allCustomers);
         return mav;
     }
@@ -51,7 +62,7 @@ public class MainController {
     @GetMapping("delete-customer")
     public String deleteCustomer(@RequestParam(name = "uuid") String uuid) {
         try {
-            webClientService.deleteCustomer(uuid);
+            webClientService.deleteCustomer(uuid, accessToken);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -68,7 +79,7 @@ public class MainController {
     @PostMapping("add-customer")
     public String addCustomer(@ModelAttribute("customer") Customer customer) {
         try {
-            webClientService.createCustomer(customer);
+            webClientService.createCustomer(customer, accessToken);
         } catch (Exception e) {
             e.getMessage();
         }
@@ -87,7 +98,7 @@ public class MainController {
     @PostMapping("update-customer")
     public String updateCustomer(@ModelAttribute("customer") Customer customer) {
         try {
-            webClientService.updateCustomer(customer.getUuid(), customer);
+            webClientService.updateCustomer(customer.getUuid(), customer, accessToken);
         } catch (Exception e) {
             e.getMessage();
         }
--- src/main/java/com/apiintegration/apiintegration/Service/WebClientService.java
+++ src/main/java/com/apiintegration/apiintegration/Service/WebClientService.java
@@ -7,10 +7,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Objects;
@@ -22,79 +24,52 @@ public class WebClientService {
 
     private final WebClient webClient;
 
-    private AccessToken accessToken;
-
     @Autowired
     private ObjectMapper mapper;
 
     public WebClientService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder
-                .baseUrl(BASE_URL)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
+        this.webClient = webClientBuilder.baseUrl(BASE_URL).defaultHeader(HttpHeaders.CONTENT_TYPE,
+                MediaType.APPLICATION_JSON_VALUE).build();
     }
 
-    public void fetchAccessToken(User user) throws JsonProcessingException {
+    public String fetchAccessToken(User user) throws JsonProcessingException {
         String userJSON = mapper.writeValueAsString(user);
-        String accessTokenJson = this.webClient.post()
-                .uri(FETCH_ACCESS_TOKEN_ENDPOINT)
-                .body(BodyInserters.fromValue(userJSON))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-        accessToken = mapper.readValue(accessTokenJson, AccessToken.class);
+        String accessTokenJson =
+                this.webClient.post().uri(FETCH_ACCESS_TOKEN_ENDPOINT).body(BodyInserters.fromValue(userJSON)).retrieve().onStatus(HttpStatusCode::is4xxClientError, error -> Mono.error(new RuntimeException("Error!"))).onStatus(HttpStatusCode::is5xxServerError, error -> Mono.error(new RuntimeException("Error!"))).bodyToMono(String.class).block();
+
+        AccessToken accessToken = mapper.readValue(accessTokenJson, AccessToken.class);
         accessToken.setAccess_token("Bearer " + accessToken.getAccess_token());
+        return accessTokenJson;
     }
 
-    public String createCustomer(Customer customer) throws JsonProcessingException {
+    public String createCustomer(Customer customer, AccessToken token) throws JsonProcessingException {
         String customerJSON = mapper.writeValueAsString(customer);
-        return this.webClient.post()
-                .uri(uriBuilder -> uriBuilder.path(API_ENDPOINTS)
-                        .queryParam("cmd", "create")
-                        .build())
-                .header("Authorization", accessToken.getAccess_token())
-                .body(BodyInserters.fromValue(customerJSON))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block().trim();
+        return this.webClient.post().uri(uriBuilder -> uriBuilder.path(API_ENDPOINTS).queryParam("cmd", "create").build()).header("Authorization", token.getAccess_token())
+                .body(BodyInserters.fromValue(customerJSON)).retrieve().onStatus(HttpStatusCode::is4xxClientError,
+                        error -> Mono.error(new RuntimeException("Error!"))).onStatus(HttpStatusCode::is5xxServerError, error -> Mono.error(new RuntimeException("Error!"))).bodyToMono(String.class).block().trim();
     }
 
-    public List<Customer> getAllCustomer() {
-        return this.webClient.get()
-                .uri(uriBuilder -> uriBuilder.path(API_ENDPOINTS)
-                        .queryParam("cmd", "get_customer_list")
-                        .build())
-                .header("Authorization", accessToken.getAccess_token())
-                .retrieve()
-                .bodyToFlux(Customer.class)
-                .collectList()
-                .block();
+    public List<Customer> getAllCustomer(AccessToken token) {
+        return this.webClient.get().uri(uriBuilder -> uriBuilder.path(API_ENDPOINTS).queryParam("cmd",
+                        "get_customer_list").build()).header("Authorization", token.getAccess_token())
+                .retrieve().onStatus(HttpStatusCode::is4xxClientError, error -> Mono.error(new RuntimeException(
+                        "Error!"))).onStatus(HttpStatusCode::is5xxServerError,
+                        error -> Mono.error(new RuntimeException("Error!"))).bodyToFlux(Customer.class).collectList().block().subList(0, 10);
     }
 
-    public String deleteCustomer(String uuid) {
-        return Objects.requireNonNull(this.webClient.post()
-                .uri(uriBuilder -> uriBuilder.path(API_ENDPOINTS)
-                        .queryParam("cmd", "delete")
-                        .queryParam("uuid", uuid)
-                        .build())
-                .header("Authorization", accessToken.getAccess_token())
-                .retrieve()
-                .bodyToMono(String.class)
-                .block(), "Error!").trim();
+    public String deleteCustomer(String uuid, AccessToken token) {
+        return Objects.requireNonNull(this.webClient.post().uri(uriBuilder -> uriBuilder.path(API_ENDPOINTS).queryParam("cmd", "delete").queryParam("uuid", uuid).build()).header("Authorization", token.getAccess_token())
+                .retrieve().onStatus(HttpStatusCode::is4xxClientError, error -> Mono.error(new RuntimeException(
+                        "Error!"))).onStatus(HttpStatusCode::is5xxServerError,
+                        error -> Mono.error(new RuntimeException("Error!"))).bodyToMono(String.class).block(), "Error"
+                + "!").trim();
     }
 
-    public String updateCustomer(String uuid, Customer customer) throws JsonProcessingException {
+    public String updateCustomer(String uuid, Customer customer, AccessToken token) throws JsonProcessingException {
         String customerJSON = mapper.writeValueAsString(customer);
-        return Objects.requireNonNull(this.webClient.post()
-                .uri(uriBuilder -> uriBuilder.path(API_ENDPOINTS)
-                        .queryParam("cmd", "update")
-                        .queryParam("uuid", uuid)
-                        .build())
-                .header("Authorization", accessToken.getAccess_token())
-                .body(BodyInserters.fromValue(customerJSON))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block(), "Error!").trim();
+        return Objects.requireNonNull(this.webClient.post().uri(uriBuilder -> uriBuilder.path(API_ENDPOINTS).queryParam("cmd", "update").queryParam("uuid", uuid).build()).header("Authorization", token.getAccess_token())
+                .body(BodyInserters.fromValue(customerJSON)).retrieve().onStatus(HttpStatusCode::is4xxClientError,
+                        error -> Mono.error(new RuntimeException("Error!"))).onStatus(HttpStatusCode::is5xxServerError, error -> Mono.error(new RuntimeException("Error!"))).bodyToMono(String.class).block(), "Error!").trim();
     }
 
 }
